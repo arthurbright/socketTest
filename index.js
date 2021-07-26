3941c68ff90048df6e53770a1bb2554feac1555c
arthurbright <55501753+arthurbright@users.noreply.github.com>
2021-07-26 16:12:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 //Set static folder
 //app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
 
