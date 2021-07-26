6156731c567ffd6d30087cf7b5069e40766552f9
arthurbright <55501753+arthurbright@users.noreply.github.com>
2021-07-26 15:58:59 -0400

    a

--- index.js
+++ index.js
@@ -11,18 +11,10 @@ const PORT = 3000 || process.env.PORT;
 
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
 
-
 app.get("/", (req, res)=>{
-    res.send();
+    res.send("hello my name is bob");
 });
 
-app.get("/api/getnames", (req, res)=>{
-    res.json()
-});
 
-app.post("/api/submit", (req, res) =>{
-    //req is the inputs
-    //send back data with res
-});
 
 
